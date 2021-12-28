0880d8d52f131930e4aa4665acf8813e3a258f0e
Noi8E <ftretyakov@kosmostyt.ru>
2021-12-28 21:22:11 +0300

    added tests

---
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+@Tag("Selenide Android")
+public class AndroidSelenideTests extends TestBase {
+    @Test
+    @DisplayName("Successful search in wikipedia android app")
+    void searchTest() {
+        step("Type search", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("Harry Potter");
+        });
+        step("Verify content found", () ->
+                $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+}
